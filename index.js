8da6e8af8b555a0172ac3c6818acaf27de3754ac
Yahya Asadi Sheikhi <yahya.asadi.sh@gmail.com>
2022-06-24 17:56:05 +0430

    instead of calling 6 diffrent functions now calls one function with different args and moves function calls from html to javascript

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-let homeScoreEl= document.getElementById("home-score")
-let guestScoreEl= document.getElementById("guest-score")
-
+const homeScoreEl = document.getElementById("home-score")
+const guestScoreEl = document.getElementById("guest-score")
+const addOneHomeBtn = document.getElementById("add-home-one-btn")
+const addTwoHomeBtn = document.getElementById("add-home-two-btn")
+const addThreeHomeBtn = document.getElementById("add-home-three-btn")
+const addOneGuestBtn = document.getElementById("add-quest-one-btn")
+const addTwoGuestBtn = document.getElementById("add-quest-two-btn")
+const addThreeGuestBtn = document.getElementById("add-quest-three-btn")
 
 let homeScore= 0;
 let guestScore= 0;
 
-function addOneHome(){
-	homeScore+=1
-	homeScoreEl.textContent= homeScore
-}
-function addTwoHome(){
-	homeScore+=2
-	homeScoreEl.textContent= homeScore
-}
-function addThreeHome(){
-	homeScore+=3
-	homeScoreEl.textContent= homeScore
-}
-function addOneGuest(){
-	guestScore+=1
-	guestScoreEl.textContent= guestScore
-}
-function addTwoGuest(){
-	guestScore+=2
-	guestScoreEl.textContent= guestScore
-}
-function addThreeGuest(){
-	guestScore+=3
-	guestScoreEl.textContent= guestScore
+function addScore(points, team) {
+	if (team === "home"){
+		homeScore += points
+		homeScoreEl.innerText = homeScore
+	}
+	else if (team === "guest"){
+		guestScore += points
+		guestScoreEl.innerText = guestScore
+	}
+	else{
+		console.log("bad arguements")
+	}
 }
 
+addOneHomeBtn.addEventListener("click", function(){addScore(1, "home")})
+addTwoHomeBtn.addEventListener("click", function(){addScore(2, "home")})
+addThreeHomeBtn.addEventListener("click", function(){addScore(3, "home")})
 
+addOneGuestBtn.addEventListener("click", function(){addScore(1, "guest")})
+addTwoGuestBtn.addEventListener("click", function(){addScore(2, "guest")})
+addThreeGuestBtn.addEventListener("click", function(){addScore(3, "guest")})
