5da82b6f97d40168e6bb1b11be77290b60af19f6
Yahya Asadi Sheikhi <yahya.asadi.sh@gmail.com>
2022-06-24 18:18:26 +0430

    adds reset function

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const homeScoreEl = document.getElementById("home-score")
 const guestScoreEl = document.getElementById("guest-score")
+
 const addOneHomeBtn = document.getElementById("add-home-one-btn")
 const addTwoHomeBtn = document.getElementById("add-home-two-btn")
 const addThreeHomeBtn = document.getElementById("add-home-three-btn")
+
 const addOneGuestBtn = document.getElementById("add-quest-one-btn")
 const addTwoGuestBtn = document.getElementById("add-quest-two-btn")
 const addThreeGuestBtn = document.getElementById("add-quest-three-btn")
+
+const gameReset = document.getElementById("reset-game")
 
 let homeScore= 0;
 let guestScore= 0;
@@ -31,3 +35,10 @@ addThreeHomeBtn.addEventListener("click", function(){addScore(3, "home")})
 addOneGuestBtn.addEventListener("click", function(){addScore(1, "guest")})
 addTwoGuestBtn.addEventListener("click", function(){addScore(2, "guest")})
 addThreeGuestBtn.addEventListener("click", function(){addScore(3, "guest")})
+
+gameReset.addEventListener("click", function(){
+	homeScore = 0
+	guestScore = 0
+	homeScoreEl.textContent = homeScore
+	guestScoreEl.textContent = guestScore
+})
